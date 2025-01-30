c6dc8b52bd6548ee5558479e7e582981561af98f
Nathan Lima <nathanls.swe@gmail.com>
2025-01-30 07:57:16 -0300

    feat: generate random adjective word

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
+const reactDescriptions = ["Fundamental", "Crucial", "Core"];
+
+function genRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
+
 function Header() {
+  const adjective = reactDescriptions[genRandomInt(reactDescriptions.length)];
+
   return (
     <header>
       <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
       <h1>React Essentials</h1>
       <p>
-        Fundamental React concepts you will need for almost any app you are
+        {adjective} React concepts you will need for almost any app you are
         going to build!
       </p>
     </header>
@@ -23,6 +31,3 @@ function App() {
 }
 
 export default App;
-
-
-//Creates separate Header component
