9ba9e9e1e9faef60967c2da07e5ab389b3e38eb9
Nathan Lima <nathanls.swe@gmail.com>
2025-02-03 16:58:08 -0300

    refactor: wrap with Section component

--- src/components/CoreConcepts.jsx
+++ src/components/CoreConcepts.jsx
@@ -1,15 +1,15 @@
 import { CORE_CONCEPTS } from "../data.js";
 import ListComponent from "./ListComponent.jsx";
+import Section from "./Section.jsx";
 
 export default function CoreConcepts() {
   return (
-    <section id="core-concepts">
-      <h2>Core Concepts</h2>
+    <Section title="Core Concepts" id="core-concepts">
       <ul>
         {CORE_CONCEPTS.map((obj, index) => (
           <ListComponent key={index} {...obj} />
         ))}
       </ul>
-    </section>
+    </Section>
   );
 }
--- src/components/Examples.jsx
+++ src/components/Examples.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 
 import TabButton from "../components/TabButton.jsx";
+import Section from "./Section.jsx";
 import { EXAMPLES } from "../data.js";
 
 export default function Examples() {
@@ -11,8 +12,7 @@ export default function Examples() {
   }
 
   return (
-    <section id="examples">
-      <h2>Examples</h2>
+    <Section title="Examples" id="examples">
       <menu>
         <TabButton
           isSelected={selectedTopic === "components"}
@@ -51,6 +51,6 @@ export default function Examples() {
       ) : (
         "Please select a topic."
       )}
-    </section>
+    </Section>
   );
 }
