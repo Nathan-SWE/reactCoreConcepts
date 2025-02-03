c636a334250765cfe81c722d47cad062bd3f8a07
Nathan Lima <nathanls.swe@gmail.com>
2025-02-03 15:33:12 -0300

    refactor: create separate files to CoreConcepts and Examples components

--- src/App.jsx
+++ src/App.jsx
@@ -1,70 +1,13 @@
-import { useState } from "react";
-
 import Header from "./components/Header/Header.jsx";
-import ListComponent from "./components/ListComponent.jsx";
-import TabButton from "./components/TabButton.jsx";
-import { CORE_CONCEPTS, EXAMPLES } from "./data.js";
+import CoreConcepts from "./components/CoreConcepts.jsx";
+import Examples from "./components/Examples.jsx";
 
 function App() {
-  const [selectedTopic, setSelectedTopic] = useState();
-
-  function handleSelect(selectedButton) {
-    setSelectedTopic(selectedButton);
-  }
-
   return (
     <>
       <Header />
-      <section>
-        <div id="core-concepts">
-          <h2>Core Concepts</h2>
-          <ul>
-            {CORE_CONCEPTS.map((obj, index) => (
-              <ListComponent key={index} {...obj} />
-            ))}
-          </ul>
-        </div>
-      </section>
-      <section id="examples">
-        <h2>Examples</h2>
-        <menu>
-          <TabButton
-            isSelected={selectedTopic === "components"}
-            onSelect={() => handleSelect("components")}
-          >
-            Components
-          </TabButton>
-          <TabButton
-            isSelected={selectedTopic === "jsx"}
-            onSelect={() => handleSelect("jsx")}
-          >
-            JSX
-          </TabButton>
-          <TabButton
-            isSelected={selectedTopic === "props"}
-            onSelect={() => handleSelect("props")}
-          >
-            Props
-          </TabButton>
-          <TabButton
-            isSelected={selectedTopic === "state"}
-            onSelect={() => handleSelect("state")}
-          >
-            State
-          </TabButton>
-        </menu>
-        {selectedTopic ? (
-          <div id="tab-content">
-            <h3>{EXAMPLES[selectedTopic].title}</h3>
-            <p>{EXAMPLES[selectedTopic].description}</p>
-            <pre>
-              <code>{EXAMPLES[selectedTopic].code}</code>
-            </pre>
-          </div>
-        ) : (
-          "Please select a topic."
-        )}
-      </section>
+      <CoreConcepts />
+      <Examples />
     </>
   );
 }
